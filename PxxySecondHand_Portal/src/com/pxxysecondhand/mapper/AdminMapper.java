a70c85e83ad1c6c1fca1264abfc5425dbd983351
wanshaobo <1647579959@qq.com>
2019-04-25 18:58:31 +0800

    添加管理员表

---
+++ PxxySecondHand_Portal/src/com/pxxysecondhand/mapper/AdminMapper.java
@@ -0,0 +1,32 @@
+package com.pxxysecondhand.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+import com.pxxysecondhand.pojo.Admin;
+import com.pxxysecondhand.pojo.AdminExample;
+
+
+public interface AdminMapper {
+    int countByExample(AdminExample example);
+
+    int deleteByExample(AdminExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Admin record);
+
+    int insertSelective(Admin record);
+
+    List<Admin> selectByExample(AdminExample example);
+
+    Admin selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Admin record, @Param("example") AdminExample example);
+
+    int updateByExample(@Param("record") Admin record, @Param("example") AdminExample example);
+
+    int updateByPrimaryKeySelective(Admin record);
+
+    int updateByPrimaryKey(Admin record);
+}
---
+++ PxxySecondHand_Portal/src/com/pxxysecondhand/pojo/Admin.java
@@ -0,0 +1,53 @@
+package com.pxxysecondhand.pojo;
+
+public class Admin {
+    private Integer id;
+
+    private String adminname;
+
+    private String adminpassword;
+
+    private String image;
+
+    private String roleid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAdminname() {
+        return adminname;
+    }
+
+    public void setAdminname(String adminname) {
+        this.adminname = adminname == null ? null : adminname.trim();
+    }
+
+    public String getAdminpassword() {
+        return adminpassword;
+    }
+
+    public void setAdminpassword(String adminpassword) {
+        this.adminpassword = adminpassword == null ? null : adminpassword.trim();
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image == null ? null : image.trim();
+    }
+
+    public String getRoleid() {
+        return roleid;
+    }
+
+    public void setRoleid(String roleid) {
+        this.roleid = roleid == null ? null : roleid.trim();
+    }
+}
